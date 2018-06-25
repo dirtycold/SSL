42a7ed6ab535e2c7e202a0f68efa1afbf36fda4b
ZH <zhanghan@gmx.cn>
2018-06-25 13:49:39 +0800

    Refactored SSLController for signal/slot support
    with more robust start/stop signaling

--- SSLController.cpp
+++ SSLController.cpp
@@ -6,45 +6,58 @@
 class SSLController::SSLControllerPrivate
 {
     friend class SSLController;
-
     QString config;
     QProcess proc;
 };
 
-SSLController& SSLController::instance()
+SSLController::SSLController(QObject *parent)
+    :QObject(parent), p(new SSLControllerPrivate)
 {
-    static SSLController instance;
-    return instance;
+    p->proc.setProgram("/usr/local/bin/ss-local");
+    p->proc.setProcessChannelMode(QProcess::ForwardedChannels);
+
+    connect(&p->proc, &QProcess::stateChanged, [this](QProcess::ProcessState procState){
+        auto state = State (procState);
+        emit stateChanged(state);
+    });
 }
 
-void SSLController::setConfig(const QString& config)
+SSLController::~SSLController()
+{
+    stop();
+    p->proc.waitForFinished();
+    delete p;
+}
+
+void SSLController::setConfig(const QString &config)
 {
     p->config = config;
 }
 
+const QString &SSLController::config() const
+{
+    return p->config;
+}
+
+SSLController::State SSLController::state() const
+{
+    auto state = State (p->proc.state());
+    return state;
+}
+
 void SSLController::start()
 {
-    if(started())
+    if (state() == Started)
         stop();
 
     p->proc.setArguments({"-c", p->config});
     p->proc.start();
-    p->proc.waitForStarted();
+    // p->proc.waitForStarted();
 }
 
 void SSLController::stop()
 {
     p->proc.terminate();
-    p->proc.waitForFinished();
+    // p->proc.waitForFinished();
 }
 
-bool SSLController::started() const
-{
-    return (p->proc.state() != QProcess::NotRunning);
-}
-
-SSLController::SSLController(): p(new SSLControllerPrivate)
-{
-    p->proc.setProgram("ss-local");
-    p->proc.setProcessChannelMode(QProcess::ForwardedChannels);
-}
--- SSLController.h
+++ SSLController.h
@@ -1,25 +1,29 @@
 #pragma once
 
 #include <memory>
-#include <QString>
+#include <QObject>
 
-class SSLController
+class SSLController : public QObject
 {
+    Q_OBJECT
 public:
-    static SSLController& instance();
+    explicit SSLController(QObject *parent = nullptr);
+    ~SSLController();
 
     void setConfig(const QString& config);
+    const QString& config() const;
+
+    enum State {Stopped, Starting, Started};
+    State state() const;
+
+public slots:
     void start();
     void stop();
-    bool started() const;
+
+signals:
+    void stateChanged(State state);
 
 private:
-    SSLController();
-    ~SSLController() = default;
-
-    SSLController(const SSLController&) = delete;
-    SSLController& operator=(const SSLController&) = delete;
-
     class SSLControllerPrivate;
-    std::unique_ptr<SSLControllerPrivate> p;
+    SSLControllerPrivate *p;
 };
