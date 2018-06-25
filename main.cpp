dc337efb418d573a9b7662af82d83ab50cc90c97
ZH <zhanghan@gmx.cn>
2018-06-25 14:49:22 +0800

    Fixed Hi-Dpi icon display

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[])
 {
     QApplication app (argc, argv);
     app.setQuitOnLastWindowClosed(false);
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
     SSLWidget widget;
     widget.show();
     return app.exec();
