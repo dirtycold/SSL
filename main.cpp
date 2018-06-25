578b4de3359d7b781bfe70d142e502b1c920a90b
ZH <zhanghan@gmx.cn>
2018-06-25 13:57:45 +0800

    Fixed exiting upon close on MacOS

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    SSLWidget w;
-    w.show();
+    QApplication app (argc, argv);
+    app.setQuitOnLastWindowClosed(false);
+    SSLWidget widget;
+    widget.show();
     return app.exec();
 }
